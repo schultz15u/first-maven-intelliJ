00487ee3a61d35197d94297eefb104180885ee01
Claire <schultz.claire@free.fr>
2017-11-13 16:28:05 +0100

    initial commit

---
+++ src/main/java/net/tncy/sandboxCW/App.java
@@ -0,0 +1,13 @@
+package net.tncy.sandboxCW;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
